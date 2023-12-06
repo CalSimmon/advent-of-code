f2c3b1057f19f8bd6aa6ab0d750816014f5ac919
Caleb Simmons <cmsimmon175@gmail.com>
2023-12-05 19:11:21 -0700

    20231205 - Didn't want to rewrite the whole thing, but I removed a bunch of fluff, and created an ungodly monolithic parse statement.

--- 2023/day_05/solution.py
+++ 2023/day_05/solution.py
@@ -3,32 +3,28 @@ from pathlib import Path
 SCRIPT_PATH = Path(__file__).resolve().parent
 INPUT_PATH = Path(SCRIPT_PATH, "inputs/input.txt")
 # INPUT_PATH = Path(SCRIPT_PATH, "inputs/example1.txt")
-# INPUT_PATH = Path(SCRIPT_PATH, "inputs/example2.txt")
 
 def parse_input(data):
     # Split each section of the almanac into a dictionary which each heading of the secion as the key
-    # Then split all lines below each heading into a list as the value of the heading
+    # Then split all lines below each heading into a list as the value of the heading (I'm enjoying monolithic parse statements, don't judge me)
     # Return almanac dictionary
-    almanac = {}
-    data_list = [section.split(':') for section in data.split('\n\n')]
-    for section in data_list:
-        almanac[section[0].replace(' map', '')] = [line.split(' ') for line in section[1].strip().split('\n')]
-    return almanac
+    almanac = {section.split(':')[0].replace(' map', ''): [list(map(int, line.split())) for line in section.split(':')[1].strip().split('\n')] for section in data.split('\n\n')}
+    return almanac 
 
 def check_conversion_map(seed, conversion_map):
     # This checks the input conversion map and returns the altered value based on the difference between the seed value and source value
     # If not in the range, return itself
     for line in conversion_map:
-        if int(seed) >= int(line[1]) and int(seed) < (int(line[1]) + int(line[2])):
-            return (int(line[0]) + (int(seed) - int(line[1])))
-    return int(seed)
+        if seed >= line[1] and seed < (line[1] + line[2]):
+            return (line[0] + (seed - line[1]))
+    return seed
 
 def conversion_map_to_range(conversion_map):
     # Convert the input conversion map into a nested list where each item is a list of start, end, and amount to offset if matched, return sorted nested list
     # For instance, [50, 98, 2] becomes [98, 99, -48]
     conversion_list = []
     for line in conversion_map:
-        conversion_list.append([int(line[1]), (int(line[1]) + int(line[2]) - 1), (int(line[0]) - int(line[1]))])
+        conversion_list.append([line[1], (line[1] + line[2] - 1), (line[0] - line[1])])
     return sorted(conversion_list, key=lambda x: x[0])
 
 def convert_seed_range_list(seed_range, conversion_range):
@@ -36,14 +32,15 @@ def convert_seed_range_list(seed_range, conversion_range):
     seed_range_list = []
     curr_value = seed_range[0]
     for i in range(len(conversion_range)):
-        if curr_value < conversion_range[i][0]:  # If the first entry in conversion_range is higher than lowest seed, add range until that point
-            seed_range_list.append([int(curr_value), (int(conversion_range[i][0] - 1))])
-            curr_value = int(conversion_range[i][0])
-        if curr_value >= int(conversion_range[i][0]) and curr_value <= int(conversion_range[i][1]):  # If between the current conversion range, add the range, unless we are at the end of the seed range
-            max_value = int(conversion_range[i][1]) if not int(seed_range[1]) <= int(conversion_range[i][1]) else int(seed_range[1])  # Set max value to seed range end if conversion range is higher than seed range
-            seed_range_list.append([(curr_value + conversion_range[i][2]), (max_value + conversion_range[i][2])])
+        cr = conversion_range[i]
+        if curr_value < cr[0]:  # If the first entry in conversion_range is higher than lowest seed, add range until that point
+            seed_range_list.append([curr_value, (cr[0] - 1)])
+            curr_value = cr[0]
+        if curr_value >= cr[0] and curr_value <= cr[1]:  # If between the current conversion range, add the range, unless we are at the end of the seed range
+            max_value = cr[1] if not seed_range[1] <= cr[1] else seed_range[1]  # Set max value to seed range end if conversion range is higher than seed range
+            seed_range_list.append([(curr_value + cr[2]), (max_value + cr[2])])
             curr_value = max_value + 1
-        if curr_value - 1 == int(seed_range[1]):  # If we've hit the end of the seed range, break
+        if curr_value - 1 == seed_range[1]:  # If we've hit the end of the seed range, break
             break
     if curr_value < seed_range[1]:  # If we did not hit the end of the seed range, add the rest of the range
         seed_range_list.append([curr_value, seed_range[1]])
@@ -59,21 +56,18 @@ def part1(parsed_data):
             if cm != 'seeds':
                 curr_soil = check_conversion_map(curr_soil, parsed_data[cm])
         print(f"Seed {seed} has a location number of {curr_soil}")
-        if lowest is None or int(curr_soil) < lowest:
-            lowest = int(curr_soil)
+        if lowest is None or curr_soil < lowest:
+            lowest = curr_soil
 
     return lowest
 
 def part2(parsed_data):
     # Get all seed pairs and conversion maps, and run through each seed pair to find the lowest location number within the range
     # Return the lowest location number of all seed ranges
-    seeds = parsed_data['seeds'][0]
+    seeds = list(map(int, parsed_data['seeds'][0]))
     lowest = None
-    seed_pairs = [[int(seeds[i]), (int(seeds[i]) + int(seeds[i + 1]) - 1)] for i in range(0, len(seeds), 2)]  # Convert the pairs to a range of seed values
-    conversion_maps = [cm for cm in parsed_data.keys() if cm != 'seeds']
-    conversion_range_dict = {}
-    for cm in conversion_maps:  # Convert the conversion maps into a dict of conversion ranges based on the map
-        conversion_range_dict[cm] = conversion_map_to_range(parsed_data[cm])
+    seed_pairs = [[seeds[i], (seeds[i] + seeds[i + 1] - 1)] for i in range(0, len(seeds), 2)]  # Convert the pairs to a range of seed values
+    conversion_range_dict = {cm: conversion_map_to_range(parsed_data[cm]) for cm in parsed_data.keys() if cm != 'seeds'}
     for seed_range in seed_pairs:
         curr_seed_range = [seed_range.copy()]  # Convert the first seed pair into a nested list so the future loops will function properly
         for curr_conv_map in conversion_range_dict.keys():
